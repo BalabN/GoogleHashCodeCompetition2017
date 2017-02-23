f926d10fab41260b50bee9ae5c43a06650a9fb57
Nikola Balaban <nikola.balaban1@gmail.com>
2017-02-23 22:09:34 +0100

    use neck

--- HashCode2017/src/Main.java
+++ HashCode2017/src/Main.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import Models.CacheServer;
@@ -19,16 +22,17 @@ public class Main {
             e.printStackTrace();
         }
 
+        System.out.println(dc.getEndpoints().size());
         for (Endpoint endpoint : dc.getEndpoints()) {
             if (endpoint.getCss().size() == 0) {
                 continue;
             }
-            for (Map.Entry<Video, Integer> entry : endpoint.getVideoRequests().entrySet()) {
-                System.out.println(entry.getKey() + "/" + entry.getValue());
-                for (Pair<CacheServer, Integer> csData : endpoint.getCss()) {
-                    
-                }
-            }
+//            for (Map.Entry<Video, Integer> entry : endpoint.getVideoRequests().entrySet()) {
+//                System.out.println(entry.getKey() + "/" + entry.getValue());
+                CacheServer minCs = getSmallestLatency(endpoint);
+                System.out.println("Smallest latency server index = " + minCs.getIndex());
+                System.out.println("Smallest latency endpoint = " + endpoint.getIndex());
+//            }
         }
 
 //        if (dc.getCs(csData.first.getIndex()).canAddVideo(entry.getKey())) {
@@ -36,5 +40,17 @@ public class Main {
 //        } else {
 //            continue;
 //        }
+    }
+
+    public static CacheServer getSmallestLatency(Endpoint endpoint) {
+        List<Integer> latencies = new ArrayList<>();
+        List<CacheServer> css = new ArrayList<>();
+
+        for (Pair<CacheServer, Integer> csData : endpoint.getCss()) {
+            latencies.add(csData.second);
+            css.add(csData.first);
+        }
+        int minIndex = latencies.indexOf(Collections.min(latencies));
+        return endpoint.getCss().get(minIndex).first;
     }
 }
--- HashCode2017/src/fillDataToDC.java
+++ HashCode2017/src/fillDataToDC.java
@@ -12,95 +12,89 @@ import Models.Video;
 
 public class fillDataToDC {
 
-	static String readFile(String fileName) throws IOException {
-	    BufferedReader br = new BufferedReader(new FileReader(fileName));
-	    try {
-	        StringBuilder sb = new StringBuilder();
-	        String line = br.readLine();
+    static String readFile(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
 
-	        while (line != null) {
-	            sb.append(line);
-	            sb.append("\n");
-	            line = br.readLine();
-	        }
-	        return sb.toString();
-	    } finally {
-	        br.close();
-	    }
-	}
-	
-	public static void fillDataToDCfromFile(String fileName) throws IOException{
-		DataCenter dc = DataCenter.getInstance();
-		String input = readFile("Items/"+fileName);
-		
-		String[] inputArray = input.split("\n");
-		
-		
-		//Basic data
-		String[] params = inputArray[0].split(" ");
-		
-		int numberOfVideos = Integer.parseInt(params[0]);
-		int numberOfEndpoints = Integer.parseInt(params[1]);
-		int numberOfRequests = Integer.parseInt(params[2]);
-		int numberOfCaches = Integer.parseInt(params[3]);
-		int capacityOfCaches = Integer.parseInt(params[4]);
-		
-		int counterOfEndpoints = 0;
-		int nextEndpointJump = 0;
-		int counterOfRequests = 0;
+            while (line != null) {
+                sb.append(line);
+                sb.append("\n");
+                line = br.readLine();
+            }
+            return sb.toString();
+        } finally {
+            br.close();
+        }
+    }
 
-		
-		//Video sizes MB
-		List<Video> videos = new ArrayList<Video>();
-		
-		for (String string : params[1].split(" ")) {
-			Video video = new Video();
-			video.setSize(Integer.parseInt(string));
-			videos.add(video);
-			
-			dc.setVideos(videos);
-		}
-		
-		//endpoints
-		
-		while(counterOfEndpoints < numberOfEndpoints){
-			System.out.println("counterOfEndpoints"+counterOfEndpoints);
-			System.out.println(numberOfEndpoints);
-			
-			Endpoint endpoint = new Endpoint();
-			
-			params = inputArray[2+nextEndpointJump].split(" ");
-			
-			endpoint.setLatencyFromDS(Integer.parseInt(params[0]));
-			int NumberOfCahceConnections = Integer.parseInt(params[1]);
-			
-			int tmpCachceConnectionsCounter = 0;
-			
-			while(tmpCachceConnectionsCounter < NumberOfCahceConnections){
-				System.out.println("counter");
-				System.out.println(2+nextEndpointJump+1+tmpCachceConnectionsCounter);
-				
-				System.out.println("number cahce:"+NumberOfCahceConnections);
-				params = inputArray[2+nextEndpointJump+1+tmpCachceConnectionsCounter].split(" ");
-				
-				CacheServer cacheserver = new CacheServer();
-				cacheserver.setIndex(Integer.parseInt(params[0]));
+    public static void fillDataToDCfromFile(String fileName) throws IOException {
+        DataCenter dc = DataCenter.getInstance();
+        String input = readFile("Items/" + fileName);
 
-				endpoint.addCs(new Pair<>(cacheserver,Integer.parseInt(params[1])));
-				
-				
-				tmpCachceConnectionsCounter++;
-			}
-			
-			nextEndpointJump = nextEndpointJump + tmpCachceConnectionsCounter + 1;
-			counterOfEndpoints++;
-		}
-		
-		//request
-		while(counterOfRequests < numberOfRequests){
-			params = inputArray[2+nextEndpointJump].split(" ");
-			
-		}
-		
-	}
+        String[] inputArray = input.split("\n");
+
+        //Basic data
+        String[] params = inputArray[0].split(" ");
+
+        int numberOfVideos = Integer.parseInt(params[0]);
+        int numberOfEndpoints = Integer.parseInt(params[1]);
+        int numberOfRequests = Integer.parseInt(params[2]);
+        int numberOfCaches = Integer.parseInt(params[3]);
+        int capacityOfCaches = Integer.parseInt(params[4]);
+
+        int counterOfEndpoints = 0;
+        int nextEndpointJump = 0;
+        int counterOfRequests = 0;
+
+        //Video sizes MB
+        List<Video> videos = new ArrayList<Video>();
+
+        String[] split = params[1].split(" ");
+        for (int i = 0; i < split.length; i++) {
+            String string = split[i];
+            Video video = new Video();
+            video.setSize(Integer.parseInt(string));
+            video.setIndex(i);
+            videos.add(video);
+
+            dc.setVideos(videos);
+        }
+
+        //endpoints
+        while (counterOfEndpoints < numberOfEndpoints) {
+            Endpoint endpoint = new Endpoint();
+            endpoint.setIndex(counterOfEndpoints);
+
+            params = inputArray[2 + nextEndpointJump].split(" ");
+
+            endpoint.setLatencyFromDS(Integer.parseInt(params[0]));
+            int NumberOfCahceConnections = Integer.parseInt(params[1]);
+
+            int tmpCachceConnectionsCounter = 0;
+
+            while (tmpCachceConnectionsCounter < NumberOfCahceConnections) {
+                params = inputArray[2 + nextEndpointJump + 1 + tmpCachceConnectionsCounter].split(" ");
+
+                CacheServer cacheserver = new CacheServer();
+                cacheserver.setIndex(Integer.parseInt(params[0]));
+
+                endpoint.addCs(new Pair<>(cacheserver, Integer.parseInt(params[1])));
+
+                tmpCachceConnectionsCounter++;
+            }
+
+            nextEndpointJump = nextEndpointJump + tmpCachceConnectionsCounter + 1;
+            counterOfEndpoints++;
+            dc.addEndpoint(endpoint);
+        }
+
+        //request
+        //		while(counterOfRequests < numberOfRequests){
+        //			params = inputArray[2+nextEndpointJump].split(" ");
+        //
+        //		}
+
+    }
 }
